36957c5d26557c48d9c90c8e90ac337a1e232326
Jauhar <siddmohamed19@gmail.com>
2020-09-30 18:24:24 +0530

    completed 3rd stage

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Date
-from datetime import datetime
+from datetime import datetime, timedelta
 engine = create_engine('sqlite:///todo.db?check_same_thread=False')
 
 Base = declarative_base()
@@ -13,7 +13,11 @@ class Task(Base):
     deadline = Column(Date, default=datetime.today())
 
     def __repr__(self):
+        return self.task + str(self.deadline)
+    def __str__(self):
         return self.task
+    def dead_line(self):
+        return self.deadline
 
 
 Base.metadata.create_all(engine)
--- todolist.py
+++ todolist.py
@@ -6,8 +6,9 @@ session = Session()
 
 
 def todays_tasks():
-    print("Today:")
-    tasks = session.query(db.Task).all()
+    today = db.datetime.today()
+    print("Today", today.day, today.strftime('%b'), ':')
+    tasks = session.query(db.Task).filter(db.Task.deadline == today.date()).all()
     if len(tasks) == 0:
         print("Nothing to do")
         return
@@ -15,21 +16,66 @@ def todays_tasks():
         i = 1
         for task in tasks:
             print(str(i) + '.', task)
+            i += 1
+        return
+def weeks_tasks():
+    today = db.datetime.today()
+    # a_week = today + db.timedelta(days=6)
+
+    for i in range(7):
+        this_day = today + db.timedelta(days=i)
+        print()
+        print(this_day.strftime('%A'), this_day.day, this_day.strftime('%b'))
+        tasks = session.query(db.Task).filter(db.Task.deadline == this_day.date()).all()
+        # print(tasks)
+        if len(tasks) == 0:
+            print("Nothing to do!")
+        else:
+            i = 1
+            for task in tasks:
+                # if task.dead_line() == this_day.date():
+                print(str(i) + '.', task)
+                i += 1
+
+
+def all_tasks():
+    print('All tasks:')
+    tasks = session.query(db.Task).order_by(db.Task.deadline).all()
+    if len(tasks) == 0:
+        print('No task to do')
+        return
+    else:
+        i = 1
+        for task in tasks:
+            print(str(i) + '. ', task, '. ', task.dead_line().day, ' ', task.dead_line().strftime('%b'), sep='')
+            i += 1
+    return
+
 def add_task():
     new_task = input('Enter task\n')
-    new_task = db.Task(task=new_task)
+    deadline = input('Enter deadline\n')
+    deadline = [int(x) for x in deadline.split('-')]
+    deadline = db.datetime(deadline[0], deadline[1], deadline[2])
+    new_task = db.Task(task=new_task, deadline=deadline)
     session.add(new_task)
     session.commit()
 
 while True:
     print('''1) Today's tasks
-2) Add task
+2) Week's tasks
+3) All tasks
+4) Add task
 0) Exit''')
     option = int(input())
     if option == 0:
+        print('Bye!')
         break
     if option == 1:
         todays_tasks()
-    if option ==2:
+    if option == 2:
+        weeks_tasks()
+    if option == 3:
+        all_tasks()
+    if option ==4:
         add_task()
 
